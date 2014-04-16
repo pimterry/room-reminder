4edfd92d3754d00c1ad87de9d14a37edb7581d5d
Tim Perry <tim.perry@softwire.com>
2014-04-16 13:15:34 +0100

    Fix bug in inconsistent date parsing, and improve error handling when sending messages

--- room_reminders/main.py
+++ room_reminders/main.py
@@ -2,8 +2,11 @@ import os
 import cherrypy
 import hipchat
 import redis
+from urllib.error import HTTPError
 from datetime import datetime, timedelta
-from flask import Flask, json, render_template, request
+from flask import Flask, json, render_template, request, abort
+
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S.%f"
 
 def build_app():
     app = Flask("Pipeline Notifier")
@@ -17,7 +20,7 @@ def build_app():
 
     # Set to 8am initially, so we end up pinging at around 8am every day.
     eight_am_today_utc = datetime.utcnow().replace(hour=8, minute=0, second=0, microsecond=0)
-    redis_conn.set('last_reminder_time', eight_am_today_utc)
+    redis_conn.set('last_reminder_time', eight_am_today_utc.strftime(DATE_FORMAT))
 
     @app.route("/", methods=["GET"])
     def show_set_reminder():
@@ -32,29 +35,36 @@ def build_app():
     @app.route("/ping")
     def maybe_send_reminder():
       try:
-        last_reminder_time = datetime.strptime(redis_conn.get('last_reminder_time').decode("UTF-8"), "%Y-%m-%d %H:%M:%S")
+        last_reminder_time = datetime.strptime(redis_conn.get('last_reminder_time').decode("UTF-8"), DATE_FORMAT)
+        next_reminder_time = last_reminder_time + timedelta(days=1)
+        
         current_reminder = redis_conn.get('reminder')
       except Exception as e:
-        print("Failed to get time and reminder from Redis: %s" % (e,))
-        current_reminder = None
-        last_reminder_time = None
+        return ("Failed to get time and reminder from Redis: %s" % (e,)), 500
 
-      if (current_reminder and
-          last_reminder_time + timedelta(days=1) <= datetime.utcnow()):     
+      if not current_reminder:
+        return "No reminder set", 503
+        
+      elif (next_reminder_time > datetime.utcnow()):
+        return "Reminder not necessary until %s" % (next_reminder_time,)
+      
+      else:
         hipchatConn = hipchat.HipChat(token=hipchat_token)
-        hipchatConn.method(url='rooms/message', method='POST', parameters={
-            'room_id': hipchat_room,
-            'from': 'Daily Reminder',
-            'message_format': 'html',
-            'notify': False,
-            'color': 'purple',
-            'message': current_reminder
-        })
+        try:
+            hipchatConn.method(url='rooms/message', method='POST', parameters={
+                'room_id': hipchat_room,
+                'from': 'Daily Reminder',
+                'message_format': 'html',
+                'notify': False,
+                'color': 'purple',
+                'message': current_reminder
+            })
+        except HTTPError as e:
+            print("Error writing to hipchat: %s" % e)
+            return "Failed to send message", 500
 
-        redis_conn.set('last_reminder_time', datetime.utcnow())        
-        return "sent"
-
-      return "no reminder set"
+        redis_conn.set('last_reminder_time', datetime.utcnow().strftime(DATE_FORMAT))
+        return "Reminder successfully sent"
     
     return app
 
